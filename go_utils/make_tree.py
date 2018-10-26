3caa650f60afc55ecdce72d5615e70e97b3d9a8e
Valentin Joly <valentin.joly@gmail.com>
2018-10-26 14:54:05 -0400

    Finished make_tree.py

--- go_utils/make_tree.py
+++ go_utils/make_tree.py
@@ -4,7 +4,8 @@ import argparse
 import csv
 import sys
 
-from vjoly import check_file_arg, open_file, print_stderr, STDIO_PATH
+from vjoly import (check_file_arg, open_file, print_stderr,
+                   updict_append_to_list, STDIO_PATH)
 
 
 PARENT_PREFIXES = [
@@ -28,16 +29,24 @@ CHILD_PREFIXES = [
 INPUT_PATH = STDIO_PATH
 OUTPUT_PATH = STDIO_PATH
 
+TOP_GO_IDS = ('GO:0008150', 'GO:0003674', 'GO:0005575')
+
 
 def add_arguments(parser):
     parser.set_defaults(prog=parser.prog, check=check_arguments, process=main)
-    parser.add_argument(
-        'input_path', metavar='FILE', default=INPUT_PATH,
+
+    input_group = parser.add_argument_group(title='Input options')
+    input_group.add_argument(
+        'input_path', metavar='PATH', default=INPUT_PATH,
         help='Input OBO file. [{}]'.format(INPUT_PATH))
-    parser.add_argument(
-        '-o', '--output_path', metavar='FILE', default=OUTPUT_PATH,
+
+    output_group = parser.add_argument_group(title='Output options')
+    output_group.add_argument(
+        '-o', '--output_path', metavar='PATH', default=OUTPUT_PATH,
         help='Output GO tree file. [{}]'.format(OUTPUT_PATH))
-    parser.add_argument(
+
+    other_group = parser.add_argument_group(title='Other options')
+    other_group.add_argument(
         '-h', '--help', action='help',
         help='Print this help page and exit.')
 
@@ -45,7 +54,8 @@ def add_arguments(parser):
 def check_arguments(args):
     errors = []
     errors += check_file_arg(args.input_path, stdio_allowed=True)
-    errors += check_file_arg(args.output_path, mode='w', stdio_allowed=True)
+    errors += check_file_arg(args.output_path, mode='w', stdio_allowed=True,
+                             prefix='-o/--output_path')
     if errors:
         for error in errors:
             print_stderr(error, prefix='ERROR')
@@ -53,8 +63,8 @@ def check_arguments(args):
 
 
 def import_obo(obo_path):
-
-    terms, types, alt, alt_ids, obsolete, parents = {}, {}, {}, {}, {}, {}
+    terms, types, alt, alt_ids, obsolete = {}, {}, {}, {}, {}
+    parents, children = {}, {}
 
     with open_file(obo_path, 'r') as obo_file:
 
@@ -68,7 +78,6 @@ def import_obo(obo_path):
                 go_id = line[4:]
                 obsolete[go_id] = False
                 alt[go_id] = False
-                parents[go_id] = []
                 alt_ids[go_id] = []
 
             elif line.startswith('name: '):
@@ -83,33 +92,32 @@ def import_obo(obo_path):
                 if '!' in alt_id:
                     alt_id = alt_id[:alt_id.index('!')]
                 alt_id = alt_id.strip()
-                alt_ids[go_id].append(alt_id)
                 alt[alt_id] = True
+                updict_append_to_list(alt_ids, go_id, alt_id)
 
             else:
                 for prefix in PARENT_PREFIXES:
                     if line.startswith(prefix):
-                        go_parent = line[len(prefix) - 2:]
-                        if '!' in go_parent:
-                            go_parent = go_parent[:go_parent.index('!')]
-                        go_parent = go_parent.strip()
-                        parents[go_id].append(go_parent)
+                        parent = line[len(prefix) - 2:]
+                        if '!' in parent:
+                            parent = parent[:parent.index('!')]
+                        parent = parent.strip()
+                        updict_append_to_list(parents, go_id, parent)
+                        updict_append_to_list(children, parent, go_id)
 
                 for prefix in CHILD_PREFIXES:
                     if line.startswith(prefix):
-                        go_child = line[len(prefix) - 2:]
-                        if '!' in go_child:
-                            go_child = go_child[:go_child.index('!')]
-                        go_child = go_child.strip()
-                        if go_child not in parents:
-                            parents[go_child] = []
-                        parents[go_child].append(go_id)
+                        child = line[len(prefix) - 2:]
+                        if '!' in child:
+                            child = child[:child.index('!')]
+                        child = child.strip()
+                        updict_append_to_list(parents, child, go_id)
+                        updict_append_to_list(children, go_id, child)
 
-    return terms, types, alt, alt_ids, obsolete, parents
+    return terms, types, alt, alt_ids, obsolete, parents, children
 
 
 def find_ancestors(parents):
-
     parents = {go_id: list(set(parents[go_id])) for go_id in parents}
 
     ancestors = {}
@@ -120,7 +128,10 @@ def find_ancestors(parents):
         while len(curr_ancestors) > len(prev_ancestors):
             prev_ancestors = curr_ancestors.copy()
             for ancestor in prev_ancestors:
-                curr_ancestors.extend(parents[ancestor])
+                try:
+                    curr_ancestors.extend(parents[ancestor])
+                except KeyError:
+                    continue
             curr_ancestors = list(set(curr_ancestors))
 
         curr_ancestors = list(set(curr_ancestors))
@@ -130,56 +141,119 @@ def find_ancestors(parents):
     return ancestors, parents
 
 
-def add_alt_ids(terms, types, alt_ids, obsolete, parents,
-                ancestors):
+def eliminate_type_incongruities(types, parents, children, ancestors):
+    for go_id, go_type in types.items():
+        p = parents.get(go_id, None)
+        if p is not None:
+            parents[go_id] = [i for i in p if types[i] == go_type]
 
+        c = children.get(go_id, None)
+        if c is not None:
+            children[go_id] = [i for i in c if types[i] == go_type]
+
+        a = ancestors.get(go_id, None)
+        if a is not None:
+            ancestors[go_id] = [i for i in a if types[i] == go_type]
+
+
+def assign_level(go_id, children, levels):
+    try:
+        for child in children[go_id]:
+            try:
+                levels[child] = max(levels[child], levels[go_id] + 1)
+            except KeyError:
+                levels[child] = levels[go_id] + 1
+            assign_level(child, children, levels)
+    except KeyError:
+        return
+
+
+def compute_go_levels(children, top_go_ids):
+    levels = {}
+    for top_go_id in top_go_ids:
+        levels[top_go_id] = 1
+        assign_level(top_go_id, children, levels)
+    return levels
+
+
+def add_alt_ids(terms, types, levels, obsolete, alt_ids,
+                children, parents, ancestors):
+    main_ids = {}
     for go_id in alt_ids:
         for alt_id in alt_ids[go_id]:
+            main_ids[alt_id] = go_id
             terms[alt_id] = terms[go_id]
             types[alt_id] = types[go_id]
             obsolete[alt_id] = obsolete[go_id]
-            parents[alt_id] = parents[go_id].copy()
-            ancestors[alt_id] = ancestors[go_id].copy()
+            try:
+                levels[alt_id] = levels[go_id]
+            except KeyError:
+                pass
+            for d in (children, parents, ancestors):
+                try:
+                    d[alt_id] = d[go_id].copy()
+                except KeyError:
+                    pass
+    return main_ids
 
-    return terms, types, obsolete, parents, ancestors
 
-
-def export_tree(tree_path, terms, types, alt, obsolete, parents, ancestors):
+def export_tree(tree_path, terms, types, levels, obsolete, alt, main_ids,
+                children, parents, ancestors):
 
     with open_file(tree_path, 'w') as tree_file:
         tree_table = csv.writer(tree_file, dialect='excel-tab')
 
-        header = ["#GO_ID", "term", "type", "is_alternative", "is_obsolete",
-                  "parents", "ancestors"]
+        header = ["#GO_ID", "term", "type", "level", "is_obsolete",
+                  "is_alternative", "main_id", "children", "parents",
+                  "ancestors"]
         tree_table.writerow(header)
 
         for go_id in sorted(terms):
+            try:
+                level = levels[go_id]
+            except KeyError:
+                level = 'NA'
+            try:
+                main_id = main_ids[go_id]
+            except KeyError:
+                main_id = ''
             row = [
                 go_id,
                 terms[go_id],
                 types[go_id],
-                str(alt[go_id]),
+                level,
                 str(obsolete[go_id]),
-                ', '.join(sorted(parents[go_id])),
-                ', '.join(sorted(ancestors[go_id]))
+                str(alt[go_id]),
+                main_id
             ]
+            for d in [children, parents, ancestors]:
+                try:
+                    row.append(', '.join(sorted(d[go_id])))
+                except KeyError:
+                    row.append('')
             tree_table.writerow(row)
 
 
 def main(args):
+    terms, types, alt, alt_ids, obsolete, parents, children = import_obo(
+        args.input_path)
 
-    terms, types, alt, alt_ids, obsolete, parents = import_obo(args.input_path)
     ancestors, parents = find_ancestors(parents)
-    terms, types, obsolete, parents, ancestors = add_alt_ids(
-        terms, types, alt_ids, obsolete, parents, ancestors)
-    export_tree(args.output_path, terms, types, alt, obsolete,
-                parents, ancestors)
+    eliminate_type_incongruities(types, parents, children, ancestors)
+    levels = compute_go_levels(children, TOP_GO_IDS)
+
+    main_ids = add_alt_ids(
+        terms, types, levels, obsolete, alt_ids, children, parents, ancestors)
+
+    export_tree(
+        args.output_path, terms, types, levels, obsolete, alt, main_ids,
+        children, parents, ancestors)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description='Make GO tree file', add_help=False)
+        description='Make GO tree file.', add_help=False)
     add_arguments(parser)
     args = parser.parse_args()
     args.check(args)
